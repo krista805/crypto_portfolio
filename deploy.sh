60baccaa7b633de32512bc0d9c6326bd78e19a4d
Krista Simmons <kristasimmons.design@gmail.com>
2022-04-20 11:15:23 -0700

    Script to deploy

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+set -e 
+
+npm run build
+
+cd dist
+
+git init
+git add -add
+git commit -m 'New Deployment'
+git push -f git@github.com:krista805/crypto_portfolio.git master:gh-pages
+
+cd -
